e950ea12c3bf5a8964101cd797322689e3407f8e
BaHGeT <abosama150@gmail.com>
2022-08-31 13:53:56 +0200

    many Edits To makt it more functiol

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 let btns = document.querySelector('#btn1');
 let screen = document.querySelector('h1');
+let SubScreen = document.querySelector('p');
 let past = 0;
 let currnt = 0;
 let operator = '';
@@ -18,31 +19,18 @@ btns.addEventListener('click', (event) =>{
     if(action === 'add' ||action === 'modul' ||action === 'subtract' 
     ||action === 'multiply' ||action === 'divide')
     {
+
         past = screen.innerText;
+
         screen.innerText=null;
         operator =key;
+        SubScreen.innerText += past+operator;
     }
 
     act(action)
 });
 
-const calculate = (n1,operator,n2) =>{
-    let result = ''
-    if (operator === '+') {
-    result = parseFloat(n1) + parseFloat(n2)
-    } else if (operator === '-') {
-    result = parseFloat(n1) - parseFloat(n2)
-    } else if (operator === '*') {
-    result = parseFloat(n1) * parseFloat(n2)
-    } else if (operator === '/') {
-    result = parseFloat(n1) / parseFloat(n2)
-    }
-    else if (operator === '%') {
-        result = parseFloat(n1) % parseFloat(n2)
-        }
-    return result
-}
-
+// Make Action Function
 const act = (action) =>{
     if (action === 'decimal') {
         if(!screen.innerText.includes('.')){screen.textContent  += '.';}
@@ -53,17 +41,38 @@ const act = (action) =>{
     }
     if (action === 'calculate') {
         currnt =screen.innerText ;
+        SubScreen.innerText += currnt;
         screen.innerText=calculate(past,operator,currnt);
+        past = '';
     }
-
+    
     if (!action) {
 
-screen.dataset.previousKey = 'number'
+        screen.dataset.previousKey = 'number'
+    }
+    
+    if (action === 'decimal') {screen.dataset.previousKey = 'decimal'}
+    
+    if (action === 'clear') {screen.dataset.previousKeyType = 'clear'}
+    
+    if (action === 'calculate') {screen.dataset.previousKeyType = 'calculate'}
 }
 
-    if (action === 'decimal') {screen.dataset.previousKey = 'decimal'}
-
-    if (action === 'clear') {screen.dataset.previousKeyType = 'clear'}
-
-    if (action === 'calculate') {screen.dataset.previousKeyType = 'calculate'}
+// Calculate Function
+const calculate = (n1,operator,n2) =>{
+        let result = ''
+        if (operator === '+') {
+        result = parseFloat(n1) + parseFloat(n2)
+        } else if (operator === '-') {
+        result = parseFloat(n1) - parseFloat(n2)
+        } else if (operator === '*') {
+        result = parseFloat(n1) * parseFloat(n2)
+        } else if (operator === '/') {
+        result = parseFloat(n1) / parseFloat(n2)
+        }
+        else if (operator === '%') {
+            result = parseFloat(n1) % parseFloat(n2)
+            }
+            SubScreen.innerText = '';
+        return result
 }
